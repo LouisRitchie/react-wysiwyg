b666b16fb7f34f533793d22244edebf0ae1c68c5
Louis Ritchie <louiscritchie@gmail.com>
2018-04-05 16:38:41 -0700

    code very bad and slow now.

--- src/components/grid/grid.jsx
+++ src/components/grid/grid.jsx
@@ -1,91 +1,61 @@
 import React, { Component } from 'react'
-import { append, concat, slice } from 'ramda'
 import { Observable } from 'rxjs'
 import { pluck, take, takeUntil } from 'rxjs/operators'
 import { Subject } from 'rxjs/Subject'
 import './styles.scss'
+import { orderCoordinateSet, isQuadrantColoured, getQuadrantBorderStyle } from './helpers.js'
 
 class Grid extends Component {
   state = {
-    coords: [[1, 1], [5, 7]]
+    coords: [[1, 1], [5, 7]],
+    selected: [-1, -1],
+    hovered: [-1, -1]
   }
 
   componentDidMount() {
     this._unmount$ = (new Subject()).pipe(take(1))
+    this._mouseMove$ = (new Subject()).pipe(pluck('target', 'dataset'), takeUntil(this._unmount$))
     this._mouseDown$ = (new Subject()).pipe(pluck('target', 'dataset'), takeUntil(this._unmount$))
     this._mouseUp$ = (new Subject()).pipe(pluck('target', 'dataset'), takeUntil(this._unmount$))
+
+    this._mouseDown$.subscribe(
+      ({x, y}) => this.setState({selected: [Number(x), Number(y)]})
+    )
 
     this._shapeDrawn$ = Observable.zip(
       this._mouseDown$,
       this._mouseUp$
     )
 
+    this._updateHover$ = Observable.zip(
+      Observable.timer(1000, 500),
+      this._mouseMove$
+    )
+
     this._shapeDrawn$.subscribe(
       ([{x: x1, y: y1}, {x: x2, y: y2}]) => {
         this.setState({
-          coords: this._orderCoordinateSet([[Number(x1), Number(y1)], [Number(x2), Number(y2)]])
+          coords: orderCoordinateSet([[Number(x1), Number(y1)], [Number(x2), Number(y2)]]),
+          selected: [-1, -1]
         })
       }
     )
+
+    this._updateHover$.subscribe(
+      ([_, {x, y}]) => this.setState({hovered: [Number(x), Number(y)]})
+    )
   }
 
-  getTLHW = (currentCoords, prevCoords) => {
-    let result = [] // [top, left, height, width]
-
-    if (currentCoords[0] > prevCoords[0]) {
-      result[1] = prevCoords[0]
-      result[3] = currentCoords[0] - prevCoords[0]
-    } else {
-      result[1] = currentCoords[0]
-      result[3] = prevCoords[0] - currentCoords[0]
-    }
-
-    if (currentCoords[1] > prevCoords[1]) {
-      result[0] = prevCoords[1]
-      result[2] = currentCoords[1] - prevCoords[1]
-    } else {
-      result[0] = currentCoords[1]
-      result[2] = prevCoords[1] - currentCoords[1]
-    }
-
-    return { top: result[0], left: result[1], height: result[2], width: result[3] }
-  }
-
-  _onMouseDown = event => this._mouseDown$.next(event)
-
-  _onMouseUp = event => this._mouseUp$.next(event)
-
-  _isQuadrantColoured = ([x, y, i]) => {
-    const [ [x1, y1], [x2, y2] ] = this.state.coords
-
-    if (x === x1 && y === y1) { return i === 4 }
-    if (x === x2 && y === y1) { return i === 3 }
-    if (x === x1 && y === y2) { return i === 2 }
-    if (x === x2 && y === y2) { return i === 1 }
-    if (x > x1 && x < x2) {
-      if (y === y1) { return ( i === 3 || i === 4 ) }
-      if (y === y2) { return ( i === 1 || i === 2 ) }
-      if (y > y1 && y < y2) { return true }
-    }
-    if (y > y1 && y < y2) {
-      if (x === x1) { return ( i === 2 || i === 4 ) }
-      if (x === x2) { return ( i === 1 || i === 3 ) }
-      if (x > x1 && x < x2) { return true }
-    }
-  }
-
-  _orderCoordinateSet = ([[x1, y1], [x2, y2]]) => {
-    if (x1 < x2 && y1 < y2) { return [[x1, y1], [x2, y2]] }
-    if (x1 > x2 && y1 < y2) { return [[x2, y1], [x1, y2]] }
-    if (x1 < x2 && y1 > y2) { return [[x1, y2], [x2, y1]] }
-    return [[x2, y2], [x1, y1]]
-  }
+  _handleMouseMove = event => this._mouseMove$.next(event)
+  _handleMouseDown = event => this._mouseDown$.next(event)
+  _handleMouseUp = event => this._mouseUp$.next(event)
 
   render() {
     return (
       <div
-        onMouseDown={this._onMouseDown}
-        onMouseUp={this._onMouseUp}
+        onMouseMove={this._handleMouseMove}
+        onMouseDown={this._handleMouseDown}
+        onMouseUp={this._handleMouseUp}
         className='gridWrapper'
         style={{ height: 20 * 20, width: 20 * 20 }}>
         {Array.apply(null, Array(21)).map((_, i) => (
@@ -96,15 +66,15 @@ class Grid extends Component {
             {Array.apply(null, Array(21)).map((_, j) => (
               <div
                 key={j}
-                className='square'
+                className={`square ${this.state.selected[0] === j && this.state.selected[1] === i ? 'selected' : ''}`}
                 style={{left: j * 20}}>
                 {Array.apply(null, Array(4)).map((_, k) => (
                   <div
                     key={k}
-                    className={`quadrant ${this._isQuadrantColoured([j, i, k + 1]) ? 'coloured' : ''}`}
+                    className={`quadrant ${isQuadrantColoured(this.state)([j, i, k + 1]) ? 'coloured' : ''}`}
                     data-x={j}
                     data-y={i}
-                    style={{left: (k % 2) * 10, top: k > 1 ? 10 : 0}} />
+                    style={{left: (k % 2) * 10, top: k > 1 ? 10 : 0, ...(this.state.selected[0] !== -1 && getQuadrantBorderStyle(this.state)([j, i, k + 1]))}} />
                 ))}
               </div>
             ))}
---
+++ src/components/grid/helpers.js
@@ -0,0 +1,75 @@
+const TOP_BORDER_STYLE = { borderTop: '1px solid blue' }
+const BOTTOM_BORDER_STYLE = { borderBottom: '1px solid blue' }
+const LEFT_BORDER_STYLE = { borderLeft: '1px solid blue' }
+const RIGHT_BORDER_STYLE = { borderRight: '1px solid blue' }
+
+export const orderCoordinateSet = ([[x1, y1], [x2, y2]]) => {
+  if (x1 < x2 && y1 < y2) { return [[x1, y1], [x2, y2]] }
+  if (x1 > x2 && y1 < y2) { return [[x2, y1], [x1, y2]] }
+  if (x1 < x2 && y1 > y2) { return [[x1, y2], [x2, y1]] }
+  return [[x2, y2], [x1, y1]]
+}
+
+export const isQuadrantColoured = state => ([x, y, i]) => {
+  const [ [x1, y1], [x2, y2] ] = state.coords
+
+  if (x === x1 && y === y1) { return i === 4 }
+  if (x === x2 && y === y1) { return i === 3 }
+  if (x === x1 && y === y2) { return i === 2 }
+  if (x === x2 && y === y2) { return i === 1 }
+  if (x > x1 && x < x2) {
+    if (y === y1) { return (i === 3 || i === 4) }
+    if (y === y2) { return (i === 1 || i === 2) }
+    if (y > y1 && y < y2) { return true }
+  }
+  if (y > y1 && y < y2) {
+    if (x === x1) { return (i === 2 || i === 4) }
+    if (x === x2) { return (i === 1 || i === 3) }
+    if (x > x1 && x < x2) { return true }
+  }
+}
+
+export const getQuadrantBorderStyle = state => ([x, y, i]) => {
+  if (state.selected[0] === -1 || isNaN(state.selected[0])) {
+    return {}
+  }
+
+  console.log(state.selected, state.hovered)
+
+  const [[x1, y1], [x2, y2]] = orderCoordinateSet([state.selected, state.hovered])
+
+  if (x === x1 && y === y1) {
+    return i === 4 ? { ...TOP_BORDER_STYLE, ...LEFT_BORDER_STYLE } : {}
+  }
+
+  if (x === x2 && y === y1) {
+    return i === 3 ? { ...TOP_BORDER_STYLE, ...RIGHT_BORDER_STYLE } : {}
+  }
+
+  if (x === x1 && y === y2) {
+    return i === 2 ? { ...BOTTOM_BORDER_STYLE, ...LEFT_BORDER_STYLE } : {}
+  }
+
+  if (x === x2 && y === y2) {
+    return i === 1 ? { ...BOTTOM_BORDER_STYLE, ...RIGHT_BORDER_STYLE } : {}
+  }
+
+  if (x > x1 && x < x2) {
+    if (y === y1) {
+      return (i === 3 || i === 4) ? TOP_BORDER_STYLE : {}
+    }
+    if (y === y2) {
+      return (i === 1 || i === 2) ? BOTTOM_BORDER_STYLE : {}
+    }
+  }
+  if (y > y1 && y < y2) {
+    if (x === x1) {
+      return (i === 2 || i === 4) ? LEFT_BORDER_STYLE : {}
+    }
+    if (x === x2) {
+      return (i === 1 || i === 3) ? RIGHT_BORDER_STYLE : {}
+    }
+  }
+
+  return {}
+}
