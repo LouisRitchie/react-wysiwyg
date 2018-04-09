5956c215cd3dad90c2fe71e176ef3551e894ade4
Louis Ritchie <louiscritchie@gmail.com>
2018-04-09 16:57:52 -0700

    dynamic grid sizing using an input range.

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -1,7 +1,7 @@
 const tools = (state = { gridSize: 20 }, {type, payload}) => {
   switch (type) {
     case 'GRID_SIZE_SET':
-      return { gridSize: payload.gridSize }
+      return { gridSize: payload }
     default:
       return state
   }
